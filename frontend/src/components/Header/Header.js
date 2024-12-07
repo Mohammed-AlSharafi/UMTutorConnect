5762378fa0be29f4158f63810867426be2f34e7e
Mohammed-AlSharafi <mohammednyemen@gmail.com>
2024-12-07 18:36:41 +0800

    removed navbar on login page

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -11,7 +11,7 @@ function Header() {
   const { isAuthenticated, logout } = useAuth();
 
   return (
-    <header className={styles.header}>
+    isAuthenticated() && <header className={styles.header}>
       <a href="/" className={styles.logoBlock}>
         <div className={styles.logo}>
           <img src={gradHat} alt="UMTutorConnect Logo" className={styles.logoImage} />
