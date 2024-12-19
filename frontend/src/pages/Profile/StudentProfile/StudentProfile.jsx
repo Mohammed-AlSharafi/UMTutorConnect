7e26f6a4288ee76e1fdfe593dd6c957b2785eb1a
Heng Wen Yang <wenyang.heng@outlook.com>
2024-12-19 19:52:44 +0800

    add 'Add Student' button and logic for displaying 'Edit Profile' and 'Add Student' based on log in state

--- frontend/src/pages/Profile/StudentProfile/StudentProfile.jsx
+++ frontend/src/pages/Profile/StudentProfile/StudentProfile.jsx
@@ -1,14 +1,24 @@
 import ProfileImage from "../../../components/ProfileImage/ProfileImage";
 import styles from "./StudentProfile.module.css";
 
-export default function StudentProfile({ img, studentInfo }) {
+export default function StudentProfile({ isloggedIn, img, studentInfo }) {
+
+    function editProfile() {
+        //implement edit profile
+    }
+
+    function addStudent() {
+        //implement add student
+    }
+
     return (
         <div>
             <div className={styles.studentProfile}>
                 <ProfileImage src={img} alt="Profile Image of the student"/>
                 <h2>{studentInfo.fullName}</h2>
                 <p>{studentInfo.role}</p>
-                <button className={styles.editBtn}>Edit Profile</button>
+                {isloggedIn && <button onClick={editProfile}>Edit Profile</button>}
+                {!isloggedIn && <button onClick={addStudent}>Add Student</button>}
             </div>
 
             <div>
