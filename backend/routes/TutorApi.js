9380cff1279e2b0b2b7634a885e3ae9d18108c2d
ethancoj <152013816+ethancoj@users.noreply.github.com>
2024-12-22 02:08:04 +0800

    Update TutorApi.js with filtering backend

--- backend/routes/TutorApi.js
+++ backend/routes/TutorApi.js
@@ -132,6 +132,34 @@ router.get("/search", authMiddleware, async (req, res) => {
   }
 });
 
+// filter tutor by subjects logic
+router.get("/search", authMiddleware, async (req,res) => {
+  try {
+    const {subjects} = req.query; // frontend to send a query list of the subjects
+  
+
+  if (!subjects) {
+    return res.status(400).json({message: "Subjects are required for filtering!"});
+  }
+
+  const subjectList = subjects.split(",");
+  console.log("Filtering tutors by subjects:", subjectList);
+
+  const filteredTutors = await tutorModel.find({
+    subjects: { $in: subjectList}, 
+  });
+
+  if (filteredTutors.length == 0) {
+    return res.status(404).json({message: "No tutors found for the selected subjects"});
+  }
+  res.status(200).json(filteredTutors);
+} catch (error) {
+  console.error("Error filtering tutors:", error);
+  res.status(500).json({message: "Server error", error: error.message})
+}
+
+});
+
 // get tutor by id
 router.get("/:id", authMiddleware, async (req, res) => {
   try {
